b1de47e837528b8ba3071ce0466f895edc43ae45
Tamirlan <ryankahito@gmail.com>
2024-02-04 15:21:30 +0300

    feat(drop-menu): add menu to profile pic in header

--- src/components/header/auth-buttons/AuthButtons.tsx
+++ src/components/header/auth-buttons/AuthButtons.tsx
@@ -1,8 +1,11 @@
 import { NavLink } from 'react-router-dom';
 import s from './styles.module.css';
-import { useSelector } from 'react-redux';
-import { selectIsLogged } from '../../../store/features/authSlice';
-import { ReactElement } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  logout,
+  selectIsLogged,
+} from '../../../store/features/authSlice';
+import { ReactElement, useRef } from 'react';
 import userPNG from '../../../assets/profile-user.png';
 
 export function AuthButtons() {
@@ -12,19 +15,20 @@ export function AuthButtons() {
 
   if (isLogged) {
     content = (
-      <>
-        <NavLink
-          to={'/user'}
-          title="your profile page"
-          className={s['profile-pic-container']}
-        >
-          <img
-            className={s['profile-pic']}
-            src={userPNG}
-            alt="your profile"
-          />
-        </NavLink>
-      </>
+      <Profile />
+      // <>
+      //   <NavLink
+      //     to={'/user'}
+      //     title="your profile page"
+      //     className={s['profile-pic-container']}
+      //   >
+      //     <img
+      //       className={s['profile-pic']}
+      //       src={userPNG}
+      //       alt="your profile"
+      //     />
+      //   </NavLink>
+      // </>
     );
   } else {
     content = (
@@ -44,4 +48,39 @@ export function AuthButtons() {
   }
 
   return <div className={s.auth}>{content}</div>;
+}
+
+function Profile() {
+  const dispatch = useDispatch();
+  const handleLogout = () => {
+    dispatch(logout());
+  };
+  const ref = useRef<HTMLUListElement>(null);
+
+  const handleShow = () => {
+    console.log(ref.current);
+    if (!ref.current) {
+      return;
+    }
+
+    ref.current.classList.toggle(s.show);
+  };
+
+  return (
+    <div onClick={handleShow} className={s['profile-wrapper']}>
+      <img
+        className={s['profile-pic']}
+        src={userPNG}
+        alt="your profile picture"
+      />
+      <ul ref={ref} className={s['drop-menu']}>
+        <li className={s['drop-menu-item']}>
+          <NavLink to="/user">My profile</NavLink>
+        </li>
+        <li className={s['drop-menu-item']} onClick={handleLogout}>
+          Sign out
+        </li>
+      </ul>
+    </div>
+  );
 }
