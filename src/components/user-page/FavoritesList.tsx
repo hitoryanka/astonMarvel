f2df6bf8b697386402180438537a04248373d362
Tamirlan <ryankahito@gmail.com>
2024-02-06 15:46:42 +0300

    feat(optimization): add suspense + lazy for history and favorites

---
+++ src/components/user-page/FavoritesList.tsx
@@ -0,0 +1,18 @@
+import { useSelector } from 'react-redux';
+import { selectFavorites } from '../../store/features/userSlice';
+import { HeroCard } from '../heroes/Heroes';
+
+export default function FavoritesList() {
+  // REFACTOR if the app is no longer about heroes start from here
+  const favorites = useSelector(selectFavorites);
+
+  if (!favorites.length) {
+    return <h2>You don`t have favorite heroes yet</h2>;
+  }
+
+  const favoriteElements = favorites.map(({ id, cover, name }) => {
+    return <HeroCard key={id} id={id} cover={cover} name={name} />;
+  });
+
+  return favoriteElements;
+}
