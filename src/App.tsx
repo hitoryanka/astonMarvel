e2249e62bda8722f3330793a5d237a76510b0969
Tamirlan <ryankahito@gmail.com>
2024-02-24 11:06:53 +0300

    fix: wrap context value in useMemo

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,31 @@ import { store } from './store/store';
 import './styles.css';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router';
-import { createContext, useState } from 'react';
+import { createContext, useMemo, useState } from 'react';
 import { getTheme, useThemeUpdate } from './lib';
 
-export const themeContext = createContext<
-  [string, React.Dispatch<React.SetStateAction<'light' | 'dark'>>]
->(['light', (): void => {}]);
+type ThemeContext = [
+  'light' | 'dark',
+  React.Dispatch<React.SetStateAction<'light' | 'dark'>>,
+];
+
+export const themeContext = createContext<ThemeContext>([
+  'light',
+  (): void => {},
+]);
 
 function App() {
   const [theme, setTheme] = useState<'light' | 'dark'>(getTheme());
   useThemeUpdate(theme);
 
+  const contextValue: ThemeContext = useMemo(
+    () => [theme, setTheme],
+    [theme],
+  );
+
   return (
     <Provider store={store}>
-      <themeContext.Provider value={[theme, setTheme]}>
+      <themeContext.Provider value={contextValue}>
         <RouterProvider router={router} />
       </themeContext.Provider>
     </Provider>
